f6b93ee8e519fb89acabbf33827a73f78064e645
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2026-05-24 09:07:19 +0300

    0.5.5_8

--- wrappers/Dominum/givers/GiverAllodium.ts
+++ wrappers/Dominum/givers/GiverAllodium.ts
@@ -7,14 +7,11 @@ import {
   ContractProvider,
   Sender,
 } from '@ton/core';
-import {
-  OP_CHANGE_WHITELIST,
-  OP_SET_WALLET,
-} from '../core/op_code';
 
 export type GiverAllodiumConfig = {
-  managerAddress: Address;
-  walletAddress?: Address | null;
+  masterAddress: Address;
+  gasPoolAddress: Address;
+  jettonWalletCode: Cell;
   frsAllodiumAddress: Address;
   allodiumFoundationAddress: Address;
 };
@@ -23,104 +20,65 @@ export function giverAllodiumConfigToCell(
   config: GiverAllodiumConfig
 ): Cell {
   const targets = beginCell()
-      .storeAddress(config.frsAllodiumAddress)
-      .storeAddress(config.allodiumFoundationAddress)
-      .endCell();
+    .storeAddress(config.frsAllodiumAddress)
+    .storeAddress(config.allodiumFoundationAddress)
+    .endCell();
 
   return beginCell()
-      .storeAddress(config.managerAddress)
-      .storeAddress(config.walletAddress ?? null)
-      .storeRef(targets)
-      .endCell();
+    .storeAddress(config.masterAddress)
+    .storeAddress(config.gasPoolAddress)
+    .storeRef(config.jettonWalletCode)
+    .storeRef(targets)
+    .endCell();
 }
 
 export class GiverAllodium implements Contract {
   constructor(
-      readonly address: Address,
-      readonly init?: { code: Cell; data: Cell }
+    readonly address: Address,
+    readonly init?: {
+      code: Cell;
+      data: Cell;
+    }
   ) {}
 
   static createFromConfig(
-      config: GiverAllodiumConfig,
-      code: Cell,
-      workchain = 0
+    config: GiverAllodiumConfig,
+    code: Cell,
+    workchain = 0
   ) {
-      const data = giverAllodiumConfigToCell(config);
-      const init = { code, data };
+    const data = giverAllodiumConfigToCell(config);
+    const init = { code, data };
 
-      return new GiverAllodium(
-          contractAddress(workchain, init),
-          init
-      );
+    return new GiverAllodium(
+      contractAddress(workchain, init),
+      init
+    );
   }
 
   static createFromAddress(address: Address) {
-      return new GiverAllodium(address);
+    return new GiverAllodium(address);
   }
 
   async sendDeploy(
-      provider: ContractProvider,
-      via: Sender,
-      value: bigint
+    provider: ContractProvider,
+    via: Sender,
+    value: bigint
   ) {
-      await provider.internal(via, { value });
-  }
-
-  async sendSetWallet(
-      provider: ContractProvider,
-      via: Sender,
-      opts: {
-          value: bigint;
-          walletAddress: Address;
-          queryId?: bigint;
-      }
-  ) {
-      const body = beginCell()
-          .storeUint(OP_SET_WALLET, 32)
-          .storeUint(opts.queryId ?? 0n, 64)
-          .storeAddress(opts.walletAddress)
-          .endCell();
-
-      await provider.internal(via, {
-          value: opts.value,
-          body,
-      });
-  }
-
-  async sendChangeWhitelist(
-      provider: ContractProvider,
-      via: Sender,
-      opts: {
-          value: bigint;
-          frsAllodiumAddress: Address;
-          allodiumFoundationAddress: Address;
-          queryId?: bigint;
-      }
-  ) {
-      const body = beginCell()
-          .storeUint(OP_CHANGE_WHITELIST, 32)
-          .storeUint(opts.queryId ?? 0n, 64)
-          .storeAddress(opts.frsAllodiumAddress)
-          .storeAddress(opts.allodiumFoundationAddress)
-          .endCell();
-
-      await provider.internal(via, {
-          value: opts.value,
-          body,
-      });
+    await provider.internal(via, { value });
   }
 
   async getGiverData(provider: ContractProvider) {
-      const { stack } = await provider.get(
-          'getGiverData',
-          []
-      );
+    const { stack } = await provider.get(
+      'getGiverData',
+      []
+    );
 
-      return {
-          managerAddress: stack.readAddress(),
-          walletAddress: stack.readAddressOpt(),
-          frsAllodiumAddress: stack.readAddress(),
-          allodiumFoundationAddress: stack.readAddress(),
-      };
+    return {
+      masterAddress: stack.readAddress(),
+      gasPoolAddress: stack.readAddress(),
+      walletAddress: stack.readAddress(),
+      frsAllodiumAddress: stack.readAddress(),
+      allodiumFoundationAddress: stack.readAddress(),
+    };
   }
 }
--- wrappers/Dominum/givers/GiverDefi.ts
+++ wrappers/Dominum/givers/GiverDefi.ts
@@ -7,120 +7,81 @@ import {
   ContractProvider,
   Sender,
 } from '@ton/core';
-import {
-  OP_CHANGE_WHITELIST,
-  OP_SET_WALLET,
-} from '../core/op_code';
 
 export type GiverDefiConfig = {
-  managerAddress: Address;
-  walletAddress?: Address | null;
-  defiBankAddress: Address;
-  defiDualAddress: Address;
+  masterAddress: Address;
+  gasPoolAddress: Address;
+  jettonWalletCode: Cell;
+  marketAddress: Address;
+  foundryAddress: Address;
+  defiTreasuryAddress: Address;
 };
 
 export function giverDefiConfigToCell(
   config: GiverDefiConfig
 ): Cell {
   const targets = beginCell()
-      .storeAddress(config.defiBankAddress)
-      .storeAddress(config.defiDualAddress)
-      .endCell();
+    .storeAddress(config.marketAddress)
+    .storeAddress(config.foundryAddress)
+    .storeAddress(config.defiTreasuryAddress)
+    .endCell();
 
   return beginCell()
-      .storeAddress(config.managerAddress)
-      .storeAddress(config.walletAddress ?? null)
-      .storeRef(targets)
-      .endCell();
+    .storeAddress(config.masterAddress)
+    .storeAddress(config.gasPoolAddress)
+    .storeRef(config.jettonWalletCode)
+    .storeRef(targets)
+    .endCell();
 }
 
 export class GiverDefi implements Contract {
   constructor(
-      readonly address: Address,
-      readonly init?: { code: Cell; data: Cell }
+    readonly address: Address,
+    readonly init?: {
+      code: Cell;
+      data: Cell;
+    }
   ) {}
 
   static createFromConfig(
-      config: GiverDefiConfig,
-      code: Cell,
-      workchain = 0
+    config: GiverDefiConfig,
+    code: Cell,
+    workchain = 0
   ) {
-      const data = giverDefiConfigToCell(config);
-      const init = { code, data };
+    const data = giverDefiConfigToCell(config);
+    const init = { code, data };
 
-      return new GiverDefi(
-          contractAddress(workchain, init),
-          init
-      );
+    return new GiverDefi(
+      contractAddress(workchain, init),
+      init
+    );
   }
 
   static createFromAddress(address: Address) {
-      return new GiverDefi(address);
+    return new GiverDefi(address);
   }
 
   async sendDeploy(
-      provider: ContractProvider,
-      via: Sender,
-      value: bigint
+    provider: ContractProvider,
+    via: Sender,
+    value: bigint
   ) {
-      await provider.internal(via, { value });
-  }
-
-  async sendSetWallet(
-      provider: ContractProvider,
-      via: Sender,
-      opts: {
-          value: bigint;
-          walletAddress: Address;
-          queryId?: bigint;
-      }
-  ) {
-      const body = beginCell()
-          .storeUint(OP_SET_WALLET, 32)
-          .storeUint(opts.queryId ?? 0n, 64)
-          .storeAddress(opts.walletAddress)
-          .endCell();
-
-      await provider.internal(via, {
-          value: opts.value,
-          body,
-      });
-  }
-
-  async sendChangeWhitelist(
-      provider: ContractProvider,
-      via: Sender,
-      opts: {
-          value: bigint;
-          defiBankAddress: Address;
-          defiDualAddress: Address;
-          queryId?: bigint;
-      }
-  ) {
-      const body = beginCell()
-          .storeUint(OP_CHANGE_WHITELIST, 32)
-          .storeUint(opts.queryId ?? 0n, 64)
-          .storeAddress(opts.defiBankAddress)
-          .storeAddress(opts.defiDualAddress)
-          .endCell();
-
-      await provider.internal(via, {
-          value: opts.value,
-          body,
-      });
+    await provider.internal(via, { value });
   }
 
   async getGiverData(provider: ContractProvider) {
-      const { stack } = await provider.get(
-          'getGiverData',
-          []
-      );
+    const { stack } = await provider.get(
+      'getGiverData',
+      []
+    );
 
-      return {
-          managerAddress: stack.readAddress(),
-          walletAddress: stack.readAddressOpt(),
-          defiBankAddress: stack.readAddress(),
-          defiDualAddress: stack.readAddress(),
-      };
+    return {
+      masterAddress: stack.readAddress(),
+      gasPoolAddress: stack.readAddress(),
+      walletAddress: stack.readAddress(),
+      marketAddress: stack.readAddress(),
+      foundryAddress: stack.readAddress(),
+      defiTreasuryAddress: stack.readAddress(),
+    };
   }
 }
