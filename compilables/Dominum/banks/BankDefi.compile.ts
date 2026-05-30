4f2968ef650ddd1dbd3e013d25ad92b2141bbac8
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2026-05-30 10:14:03 +0300

    0.5.6_3

---
+++ compilables/Dominum/banks/BankDefi.compile.ts
@@ -0,0 +1,7 @@
+// compilables/Dominum/banks/BankDefi.compile.ts
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+  lang: 'tolk',
+  entrypoint: 'contracts/Dominum/banks/bank_defi.tolk',
+};
---
+++ compilables/Dominum/banks/BankDominum.compile.ts
@@ -0,0 +1,7 @@
+// compilables/Dominum/banks/BankDominum.compile.ts
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+  lang: 'tolk',
+  entrypoint: 'contracts/Dominum/banks/bank_dominum.tolk',
+};
